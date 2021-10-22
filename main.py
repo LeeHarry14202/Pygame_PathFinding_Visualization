5c3403b479189f832bd41e7506fe14ccb3f43231
LeeHarry14202 <20521912@gm.uit.edu.vn>
2021-10-22 22:45:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,26 +6,28 @@ SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
 def init_gird(rows, cols, color):
-      gird_ = np.empty((rows, cols), dtype = tuple)
+      gird_ = np.empty((rows, cols), dtype=tuple)
+      # matrix contains RBG number
       gird_.fill(color)
       return gird_ 
 
+
 def draw_gird(screen):
     for i, row in enumerate(gird):
-        for j, pixel in enumerate(row):
-            pygame.draw.rect(screen, pixel, (j * PIXEL_SIZE, i * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE))
+        for j, color in enumerate(row):
+            # Not draw first & last row and col in gird
+            if i != 0 and j != 0 and i != ROWS - 1 and j != COLS -1:
+                  pygame.draw.rect(screen, color, (j * PIXEL_SIZE, i * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE))
 
     if DRAW_GIRD_LINES:
-        # Add 1 to draw the last row
-        for i in range(ROWS + 1):
-            pygame.draw.line(screen, BLACK, start_pos=(0, i * PIXEL_SIZE),
-                             end_pos=(SCREEN_WIDTH, i * PIXEL_SIZE))
-        # Add 1 to draw the last col
-        for j in range(COLS + 1):
-            pygame.draw.line(screen, BLACK, start_pos=(j * PIXEL_SIZE, 0),
-                             end_pos=(j * PIXEL_SIZE, SCREEN_HEIGHT - abs(SCREEN_HEIGHT - SCREEN_WIDTH)))
-
-
+        for i in range(ROWS):
+            pygame.draw.line(screen, BLACK, 
+                              start_pos=(0, i * PIXEL_SIZE), 
+                              end_pos=(SCREEN_WIDTH, i * PIXEL_SIZE))
+        for j in range(COLS):
+            pygame.draw.line(screen, BLACK, 
+                              start_pos=(j * PIXEL_SIZE, 0),
+                              end_pos=(j * PIXEL_SIZE, SCREEN_HEIGHT - abs(SCREEN_HEIGHT - SCREEN_WIDTH)))
 
 def draw(screen):
       screen.fill(BACKGROUND_COLOR)
@@ -48,6 +50,7 @@ def main():
             draw(SCREEN)
 
             clock.tick(FPS)
+            
 
 if __name__ == "__main__":
       main()
